60660e9fbec1bfe9e976f447cb237ef993fdbc26
Logan Geiser <muclecake@gmail.com>
2024-10-28 23:41:46 -0400

    added pytest to requirements

--- python_package_exercise/python_package_exercise/basic_sort_UNIQUE_SUFFIX/int_sort.py
+++ python_package_exercise/python_package_exercise/basic_sort_UNIQUE_SUFFIX/int_sort.py
@@ -61,7 +61,7 @@ def insertion(int_list):
     for i in range(1, len(int_list)):
         key = int_list[i]
         j = i - 1
-        while j >= 0 and key < int_list[i]:
+        while j >= 0 and key < int_list[j]:
             int_list[j + 1] = int_list[j]
             j -=  1
         int_list[j + 1] = key
