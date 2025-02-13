6f0a1ac078a97113fc5df21986efc925340e1d17
Stephen Goodridge <sgoodridge96@gmail.com>
2025-02-13 17:55:17 -0500

    basic layout started

--- Config_Generator/config_gui.py
+++ Config_Generator/config_gui.py
@@ -1,16 +1,36 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import (
+    QApplication,
+    QWidget,
+    QLabel,
+    QComboBox,
+    QVBoxLayout,
+)
 
 
 class Config_Generator(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Config Generator")
-        self.resize(1280, 720)
-        self.show()
+        self.resize(600, 300)
+
+        self.camera_label = QLabel("Select Camera")
+        self.cam_combo_box = QComboBox(self)
+
+        self.audio_label = QLabel("Select audio device")
+        self.audio_combo_box = QComboBox(self)
+
+        gui_layout = QVBoxLayout()
+        gui_layout.addWidget(self.camera_label)
+        gui_layout.addWidget(self.cam_combo_box)
+        gui_layout.addWidget(self.audio_label)
+        gui_layout.addWidget(self.audio_combo_box)
+
+        self.setLayout(gui_layout)
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Config_Generator()
+    window.show()
     sys.exit(app.exec())
