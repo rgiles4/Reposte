95a265d92768728caa2683916be25838e8978b17
Stephen Goodridge <sgoodridge96@gmail.com>
2025-02-13 20:20:53 -0500

    ffmpeg command finds cameras

--- Config_Generator/config_gui.py
+++ Config_Generator/config_gui.py
@@ -1,13 +1,13 @@
 import sys
+import subprocess
+import imageio_ffmpeg
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
     QComboBox,
     QFormLayout,
-    QSizePolicy,
     QPushButton,
-    QHBoxLayout,
 )
 
 
@@ -38,12 +38,16 @@ class Config_Generator(QWidget):
 
         gui_layout = QFormLayout()
 
-        self.cam_combo_box.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
-        )
-        self.audio_combo_box.setSizePolicy(
-            QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
-        )
+        all_cameras = self.Get_Cameras()
+
+        # NOTE: Add back if window size is not fixed. Will expand dropdowns to
+        # fill the window
+        # self.cam_combo_box.setSizePolicy(
+        #     QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
+        # )
+        # self.audio_combo_box.setSizePolicy(
+        #     QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Fixed
+        # )
 
         self.generate_button = QPushButton("Generate Config")
 
@@ -52,6 +56,35 @@ class Config_Generator(QWidget):
         gui_layout.addWidget(self.generate_button)
 
         self.setLayout(gui_layout)
+
+    def Get_Cameras(self):
+        cameras = []
+        # TODO: Add Mac command later
+        try:
+            ffmpeg_path = imageio_ffmpeg.get_ffmpeg_exe()
+            print(f"FFmpeg path: {ffmpeg_path}")
+
+            if sys.platform == "win32":
+                command = [
+                    ffmpeg_path,
+                    "-list_devices",
+                    "true",
+                    "-f",
+                    "dshow",
+                    "-i",
+                    "dummy",
+                ]
+
+            command_result = subprocess.run(
+                command, capture_output=True, text=True
+            )
+            output = command_result.stderr
+            print(output)
+
+            # if sys.platform == "win32":
+
+        except Exception as e:
+            print(f"AYYY YO Command failed:\n {e}")
 
 
 if __name__ == "__main__":
