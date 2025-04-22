c8cb68956cfb1049c9ef9fe0856c591daad263ba
Stephen Goodridge <sgoodridge96@gmail.com>
2025-04-22 19:12:19 -0400

    format fix

--- RePoste/scoreboard_manager.py
+++ RePoste/scoreboard_manager.py
@@ -170,7 +170,7 @@ class ScoreboardManager(QObject):
 
     def _parse_sfs_link_hex(self, hex_str: str) -> dict:
         # fmt: off
-        hex_pairs = [hex_str[i : i + 2] for i in range(0, 14, 2)]
+        hex_pairs = [hex_str[i:i+2] for i in range(0, 14, 2)]
         # fmt: on
         if len(hex_pairs) != 7:
             return {}
